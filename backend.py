657541fdc34c00730112e52568818e9c06c77ccb
Ayush <z5545780@ad.unsw.edu.au>
2025-12-15 06:16:25 +1100

    wrote backend code

--- backend.py
+++ backend.py
@@ -22,3 +22,32 @@ def get_vector_store():
     vector_store = MongoDBAtlasVectorSearch(collection=collection,embedding=embeddings,index_name=ATLAS_VECTOR_SEARCH)
 
     return vector_store
+
+
+def ingest_text(text_content):
+    vector_store = get_vector_store()
+
+    # document in mongoDB => bascially a json object which gets loaded in mongoDB database
+    docs = Document(text_content)
+    vector_store.add_documents([docs])
+    return True
+
+
+def get_rag_response(query):
+    vector_store = get_vector_store()
+    llm = ChatGoogleGenerativeAI(model = "gemini-2.5-flash")
+    retriever = vector_store.as_retriever(search_type = "similarity", search_kwargs = {k:3})
+
+    prompt_template = """ Use the following context from the user in order to provide an accurate and concise response to the query.
+    If you don't know the answer, just say that you don't know, don't try to make up an answer."""
+
+    prompt = PromptTemplate(
+        template=prompt_template,
+        input_variables=["context", "question"]
+    )
+
+    qa_chain = RetrievalQA.from_chain_type(llm = llm,chain_stuff = 'stuff', retriever = retriever)
+
+    response = qa_chain.invoke({"query" : query})
+
+    return response
